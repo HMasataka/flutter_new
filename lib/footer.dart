3ddeaed9d252dfb8a6f8b1edc530e4f5a7cb5896
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2021-02-01 16:42:47 +0900

    Feat add footer content

--- lib/footer.dart
+++ lib/footer.dart
@@ -7,20 +7,36 @@ class Footer extends StatefulWidget {
   _Footer createState() => _Footer();
 }
 
-class _Footer extends State {
+class _Footer extends State<Footer> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: const [
+      type: BottomNavigationBarType.fixed,
+      items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
+          icon: Icon(Icons.business),
+          label: 'Business',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.school),
+          label: 'School',
         ),
       ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
     );
   }
 }
