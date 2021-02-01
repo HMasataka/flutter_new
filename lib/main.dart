9c85dcdba06715a590eb7573b7098b1e8e31aab3
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2021-02-01 16:04:40 +0900

    Feat split footer file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'header.dart';
+import 'footer.dart';
 
 void main() {
   runApp(App());
@@ -13,18 +14,7 @@ class App extends StatelessWidget {
       home: Scaffold(
         appBar: Header(),
         body: Center(child: Text("Test")),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-          ],
-        ),
+        bottomNavigationBar: Footer(),
       ),
     );
   }
